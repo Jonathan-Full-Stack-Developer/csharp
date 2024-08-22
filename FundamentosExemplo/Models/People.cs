f190781876c65f634d7d8822dbb5ab736431d2ee
Jonathan Lima <jon.1lima@hotmail.com>
2024-08-22 16:06:18 -0300

    feat:initial fundamentals

---
+++ FundamentosExemplo/Models/People.cs
@@ -0,0 +1,14 @@
+﻿namespace FundamentosExemplo.Models
+{
+    public class People
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public void IntroduceYourself()
+        {
+            Console.WriteLine($"Hello, my name is {Name}, i'm {Age} old!");
+        }
+    }
+}
---
+++ FundamentosExemplo/Program.cs
@@ -0,0 +1,8 @@
+﻿using FundamentosExemplo.Models;
+
+People people1 = new People();
+
+people1.Name = "Jh0w";
+people1.Age = 21;
+
+people1.IntroduceYourself();
