29762389064b0e3adfd06ff673f95d94b6aef768
Jonathan Lima <jon.1lima@hotmail.com>
2024-08-22 17:05:03 -0300

    feat:initial fundamentals

--- FundamentosExemplo/Models/People.cs
+++ FundamentosExemplo/Models/People.cs
@@ -4,11 +4,14 @@
     {
         public string Name { get; set; }
         public int Age { get; set; }
-
+        DateTime data_today = DateTime.Now;
+        DateTime data_after = DateTime.Now.AddDays(5);
+        string data = DateTime.Now.ToString("dd/MM/yyyy");
+        string time = DateTime.Now.ToString("HH:mm");
 
         public void IntroduceYourself()
         {
-            Console.WriteLine($"Hello, my name is {Name}, i'm {Age} old!");
+            Console.WriteLine($"\nHello, my name is {Name}, I'm {Age} years old!\nYou are executing this code on {data_today}.\nIf you add 5 days, the date will be {data_after}.\nBut if you prefer just the date then {data}.\nOr if you need the time, then {time}.\n");
         }
     }
 }
--- FundamentosExemplo/obj/Release/net8.0/FundamentosExemplo.AssemblyInfo.cs
+++ FundamentosExemplo/obj/Release/net8.0/FundamentosExemplo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FundamentosExemplo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eee6ca33a86c4af2a0579458c0c565c523a0ee2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f190781876c65f634d7d8822dbb5ab736431d2ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("FundamentosExemplo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FundamentosExemplo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
